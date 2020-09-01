a0b209807d0ae9409e407bab1a95b5ba8eebdea8
ccforeverd <zh1045456074@163.com>
2020-09-01 22:28:15 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <bar />
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Bar from '~/components/pc/Bar'
+
+export default Vue.extend({
+  components: {
+    Bar
+  }
+})
+</script>
